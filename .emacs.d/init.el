4241ffb514bfe0c3e55e3dc0130eefb28f7fb5cc
okertanov <okertanov@gmail.com>
2011-05-13 11:00:43 +0300

    init.el font & colors

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,26 @@
 ;; My personal emacs configuration
 ;; Oleg Kertanov (c) 2011
 
+;; UI
+;; (menu-bar-mode -1)
+(tool-bar-mode -1)
+
+(if (eq window-system 'w32)
+    (progn
+      (setq default-frame-alist
+            (append '(
+                      (width . 100) (height . 35)
+
+                      (cursor-color . "OrangeRed1")  ;; coral1
+                      (background-color . "midnight blue")
+                      ;; (background-color . "grey16")
+                      (foreground-color . "grey90")
+                      (vertical-scroll-bars . right)
+
+                      (font . "-*-Lucida Sans Unicode-normal-normal-normal-sans-*-*-*-*-p-*-iso8859-1")
+
+                      ) default-frame-alist)
+            )
+   )
+)
+
