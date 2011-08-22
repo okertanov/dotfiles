0179b1d20f55b801179a0a661c4ac48ed92ab50f
okertanov <okertanov@gmail.com>
2011-08-22 17:46:16 +0300

    conv-svn-git

---
+++ bin/conv-svn-git.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+svn_repos="bonima espectrale.com habrahabr honda-mafia nntp-reader picasa-twitpic \
+            replica.espectrale.com rss-notify twilats"
+
+for i in $svn_repos
+do
+sudo git svn clone svn+ssh://okertanov@crater.espectrale.com/var/svn/repo/trunk/$i
+sudo git clone --bare --shared file://$i $i.git
+sudo rm -rf $i
+
+sudo chown -R root:developers ./$i.git
+sudo chmod -R g+wX,o= ./$i.git
+sudo find ./$i.git -type d -exec chmod g+s '{}' ';'
+
+pushd ./
+cd ./$i.git
+git update-server-info
+popd
+done
+
+
