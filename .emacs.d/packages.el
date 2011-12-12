d83d16b7f41c8689fd43e32db1e63f580d840b32
okertanov <okertanov@gmail.com>
2011-12-12 15:25:55 +0200

    clojure-mode and paredit packages

--- .emacs.d/packages.el
+++ .emacs.d/packages.el
@@ -1,5 +1,15 @@
 ;; packages.el - ux configuration
 
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+;; For the Clojure
+(require 'clojure-mode)
+(defun turn-on-paredit () (paredit-mode 1))
+(add-hook 'clojure-mode-hook 'turn-on-paredit)
+
 ;; No version control please
 (setq vc-handled-backends ())
 
