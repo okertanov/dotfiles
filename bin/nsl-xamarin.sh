adf9414d87fb4bf48bcdab4b690fcb4da127275b
okertanov <okertanov@gmail.com>
2016-02-29 12:01:56 +0200

    Atom cfg up, cleanup, websocketd up, empty think-client

---
+++ bin/nsl-xamarin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+nslookup xamarin.com 217.21.169.19 -timeout=1 2>&1 | grep -i "SERVFAIL\|NXDOMAIN\|REFUSED\|not found\|no servers"
+
+if [ $? -eq 0 ]; then
+    echo red
+    echo -n "red" | nc -4u -w0 localhost 1738
+else
+    echo green
+    echo -n "green" | nc -4u -w0 localhost 1738
+fi
